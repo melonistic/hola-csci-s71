0287efa70585bd88231e211eaaa370b201d46511
Santiago Alvarez-Vargas <santiago.v.alvarez@gmail.com>
2018-06-26 14:28:28 -0400

    hw1 accepts arguments

--- src/edu/melonistic/Main.java
+++ src/edu/melonistic/Main.java
@@ -3,6 +3,10 @@ package edu.melonistic;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("%s Santiago, Diego, Jacob", "Hola");
+        String name = args[0];
+        if (null == name) {
+            name = ", identify";
+        }
+        System.out.printf("Hola %s!!!", args[0]);
     }
 }
